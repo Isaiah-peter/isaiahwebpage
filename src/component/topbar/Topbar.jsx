a6066d1bc8578af6150e94096d474cfbe80e1ff7
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-10 20:14:28 +0100

    topbar

--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -1,9 +1,21 @@
+import { Person, Mail } from "@material-ui/icons";
+
 const Topbar = () => {
   return (
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">dfg</a>
+          <a href="#intro" className="logo">
+            standard
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+234 810 466 3471</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>peterisaiah@gmail.com</span>
+          </div>
         </div>
         <div className="right"></div>
       </div>
