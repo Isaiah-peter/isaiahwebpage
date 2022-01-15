4d78f570f743a22ad8e47026f98c2471f9c81e88
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-15 21:59:41 +0100

    small change

--- src/component/intro/Intro.jsx
+++ src/component/intro/Intro.jsx
@@ -1,5 +1,14 @@
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="img/" alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 };
 
 export default Intro;
