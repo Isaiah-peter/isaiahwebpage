fb97154f53b38208dcd340d7c65c5d5cc1760655
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-20 07:35:46 +0100

    more

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,5 +1,42 @@
 const Portfolio = () => {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Webapp</li>
+        <li>Mobile App</li>
+        <li>Desing</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+        <div className="item">
+          <img src="img\first-work.png" alt="" />
+          <h3>first work</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
