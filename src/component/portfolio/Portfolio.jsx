e3eefbe29f1e2e0cba73a264fa4b0d34933340ae
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-21 22:55:53 +0100

    more changes

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+
 const Portfolio = () => {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "desing",
+      title: "Desing",
+    },
+    {
+      id: "branding",
+      title: "Branding",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Webapp</li>
-        <li>Mobile App</li>
-        <li>Desing</li>
-        <li>Branding</li>
+        {list.map((item) => (
+          <PortfolioList
+            key={item.id}
+            item={item}
+            active={selected === item.id}
+            setSelected={setSelected}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
@@ -23,7 +52,7 @@ const Portfolio = () => {
           <h3>first work</h3>
         </div>
         <div className="item">
-          <img src="img\first-work.png" alt="" />
+          <img src="img\second-work.png" alt="" />
           <h3>first work</h3>
         </div>
         <div className="item">
@@ -31,7 +60,7 @@ const Portfolio = () => {
           <h3>first work</h3>
         </div>
         <div className="item">
-          <img src="img\first-work.png" alt="" />
+          <img src="img\third-image.png" alt="" />
           <h3>first work</h3>
         </div>
       </div>
