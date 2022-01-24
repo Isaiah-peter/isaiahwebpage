e21342ba11c52eba881d1a0648fe7625a8cb2f9a
Peter Isaiah <peterisaiah4fun@gmail.com>
2022-01-24 05:37:58 +0100

    more

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
+import { webSite, reactApp, golangApp, workWith, mediaApp } from "../../data";
 
 const Portfolio = () => {
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
   const list = [
     {
       id: "featured",
@@ -25,6 +27,9 @@ const Portfolio = () => {
       title: "Branding",
     },
   ];
+
+  useEffect(() => {}, []);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
